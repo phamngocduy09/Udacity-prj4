2a1ab03035dfbbbeb6df5b0df2f0d48ec55b762c
phamngocduy09 <{ID}+{username}@users.noreply.github.com>
2024-08-22 13:28:32 +0700

    first commit

---
+++ starter/client/src/config.ts
@@ -0,0 +1,9 @@
+const apiId = '6qnhydisri'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/test`
+
+export const authConfig = {
+  // domain: 'dev-nd9990-p4.us.auth0.com',
+  domain: 'dev-fsn2h3bijtlmz10s.us.auth0.com',            // Auth0 domain
+  clientId: 'YclfArS4lqjHNwJuBL91jU8z9HJlXE9M',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
